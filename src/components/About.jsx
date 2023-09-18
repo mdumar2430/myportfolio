ebc812b3d78592352ab855953bcdd4d05e06206d
mdumarRatham <81101575+mdumar2430@users.noreply.github.com>
2023-09-18 16:31:55 +0530

    About Added

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='About' className='w-full h-screen bg-gradient-to-b from-gray-800 via-black to-black text-white'>
+        <div className='max-w-screen-lg p-5 mx-auto flex flex-col justify-center w-full'> 
+            <div className='pb-8'>
+                <h3 className='text-4xl font-bold inline border-b-4 border-gray-400'>About</h3>
+            </div>
+            <p className='text-2xl text-justify mt-5'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore ut deserunt nihil cum fuga porro, quam obcaecati, sed quidem, assumenda enim inventore. Nostrum, consequuntur. Dicta laudantium possimus assumenda numquam nemo illo error, voluptate mollitia reprehenderit voluptates id exercitationem tenetur provident ipsum, quisquam ex repudiandae cum accusantium nostrum. Error magnam vero inventore rem cumque? Sint, facilis commodi illum odio nihil maiores!
+            </p>
+            <br />
+            <p className='text-2xl text-justify mt-5'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit odio cupiditate nam minima officia ducimus nostrum totam numquam asperiores! Modi, nostrum perspiciatis eos a reiciendis quisquam inventore? Cumque nostrum amet blanditiis id reprehenderit quam ipsam optio itaque ut. Itaque, tempore eos adipisci cumque similique aliquid aliquam. Officia modi ipsum quisquam harum voluptatibus sunt tempore illum! Ipsum, rerum recusandae. Rerum, minima!
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
